86a6de2c74436bb650af70d60a6f52cdef262dac
Dino Smajovic <smajovic.dino@gmail.com>
2018-08-28 15:40:26 +0200

    :sparkles: Added get photos route

--- Data/SocialNetworkRepository.cs
+++ Data/SocialNetworkRepository.cs
@@ -9,154 +9,159 @@ using SocialNetwork.API.Models;
 
 namespace SocialNetwork.API.Data
 {
-    public class SocialNetworkRepository : ISocialNetworkRepository
+  public class SocialNetworkRepository : ISocialNetworkRepository
+  {
+    private readonly DataContext _context;
+
+    public SocialNetworkRepository(DataContext context)
     {
-        private readonly DataContext _context;
-
-        public SocialNetworkRepository(DataContext context)
-        {
-            _context = context;
-        }
-
-        public void Add<T>(T entity) where T : class
-        {
-            _context.Add(entity);
-        }
-
-        public void Delete<T>(T entity) where T : class
-        {
-            _context.Remove(entity);
-        }
-
-        public async Task<User> GetUser(int id)
-        {
-            var user = await _context.Users
-                .Include(p => p.Photos)
-                .FirstOrDefaultAsync(u => u.Id == id);
-
-            return user;
-        }
-        
-
-        public async Task<IEnumerable<User>> GetUsers()
-        {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
-
-            return users;
-        }
-
-        public async Task<IEnumerable<Post>> GetPosts(int id)
-        {
-            var posts = await _context.Posts
-                .Where(p => p.UserId == id)
-                .ToListAsync();
-
-            return posts;
-        }
-
-        public async Task<Post> GetPost(int id)
-        {
-            var post = await _context.Posts
-                .FirstOrDefaultAsync(p => p.Id == id);
-
-            return post;
-        }
-
-        public async Task<IEnumerable<Comment>> GetComments(int id)
-        {
-            var comments = await _context.PostComments.Where(c => c.PostId == id).ToListAsync();
-
-            return comments;
-        }
-
-        public async Task<Comment> GetComment(int id)
-        {
-            var comment = await _context.PostComments.FirstOrDefaultAsync(c => c.Id == id);
-
-            return comment;
-        }
-
-        public async Task<Post> GetPostFromComment(int id)
-        {
-            var comment = await _context.PostComments.FirstOrDefaultAsync(c => c.Id == id);
-
-            var post = await _context.Posts.FirstOrDefaultAsync(p => p.Id == comment.PostId);
-
-            return post;
-        }
-
-        public async Task<Like> GetLike(int postId, int likerId)
-        {
-            var like = await _context.PostLikes
-                .Where(l => l.LikerId == likerId)
-                .FirstOrDefaultAsync(l => l.PostId == postId);
-
-            return like;
-        }
-        public async Task<bool> SaveAll()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
-
-        public async Task<Follow> GetFollow(int userId, int recipientId)
-        {
-            return await _context.Followers.FirstOrDefaultAsync(u => u.FollowerId == userId && u.FollowedId == recipientId); 
-        }
-
-        public async Task<Event> GetEvent(int id)
-        {
-            return await _context.Events.FirstOrDefaultAsync(e => e.Id == id);
-        }
-
-        public async Task<Photo> GetPhoto(int id)
-        {
-            var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
-
-            return photo;
-        }
-
-        public async Task<Message> GetMessage(int id)
-        {
-            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
-        }
-
-        public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
-        {
-            var messages = _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .AsQueryable();
-
-            switch (messageParams.MessageContainer)
-            {
-                case "Inbox":
-                    messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.RecipientDeleted == false );
-                    break;
-                case "Outbox":
-                    messages = messages.Where(u => u.SenderId == messageParams.UserId && u.SenderDeleted == false);
-                    break;
-                default: 
-                    messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false && u.RecipientDeleted == false);
-                    break;
-            }
-
-            messages = messages.OrderByDescending(d => d.MessageSent);
-
-            return await PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
-        }
-
-        public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
-        {
-            var messages = await _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .Where(m => (m.RecipientId == userId && m.RecipientDeleted == false && m.SenderId == recipientId) 
-                    || (m.RecipientId == recipientId && m.SenderId == userId && m.SenderDeleted == false))
-                .OrderByDescending(m => m.MessageSent)
-                .ToListAsync();
-
-            return messages;
-        }
+      _context = context;
     }
 
+    public void Add<T>(T entity) where T : class
+    {
+      _context.Add(entity);
+    }
+
+    public void Delete<T>(T entity) where T : class
+    {
+      _context.Remove(entity);
+    }
+
+    public async Task<User> GetUser(int id)
+    {
+      var user = await _context.Users
+          .Include(p => p.Photos)
+          .FirstOrDefaultAsync(u => u.Id == id);
+
+      return user;
+    }
+
+
+    public async Task<IEnumerable<User>> GetUsers()
+    {
+      var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+
+      return users;
+    }
+
+    public async Task<IEnumerable<Post>> GetPosts(int id)
+    {
+      var posts = await _context.Posts
+          .Where(p => p.UserId == id)
+          .ToListAsync();
+
+      return posts;
+    }
+
+    public async Task<Post> GetPost(int id)
+    {
+      var post = await _context.Posts
+          .FirstOrDefaultAsync(p => p.Id == id);
+
+      return post;
+    }
+
+    public async Task<IEnumerable<Comment>> GetComments(int id)
+    {
+      var comments = await _context.PostComments.Where(c => c.PostId == id).ToListAsync();
+
+      return comments;
+    }
+
+    public async Task<Comment> GetComment(int id)
+    {
+      var comment = await _context.PostComments.FirstOrDefaultAsync(c => c.Id == id);
+
+      return comment;
+    }
+
+    public async Task<Post> GetPostFromComment(int id)
+    {
+      var comment = await _context.PostComments.FirstOrDefaultAsync(c => c.Id == id);
+
+      var post = await _context.Posts.FirstOrDefaultAsync(p => p.Id == comment.PostId);
+
+      return post;
+    }
+
+    public async Task<Like> GetLike(int postId, int likerId)
+    {
+      var like = await _context.PostLikes
+          .Where(l => l.LikerId == likerId)
+          .FirstOrDefaultAsync(l => l.PostId == postId);
+
+      return like;
+    }
+    public async Task<bool> SaveAll()
+    {
+      return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Follow> GetFollow(int userId, int recipientId)
+    {
+      return await _context.Followers.FirstOrDefaultAsync(u => u.FollowerId == userId && u.FollowedId == recipientId);
+    }
+
+    public async Task<Event> GetEvent(int id)
+    {
+      return await _context.Events.FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    public async Task<Photo> GetPhoto(int id)
+    {
+      var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
+
+      return photo;
+    }
+
+    public async Task<Message> GetMessage(int id)
+    {
+      return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+    }
+
+    public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
+    {
+      var messages = _context.Messages
+          .Include(u => u.Sender).ThenInclude(p => p.Photos)
+          .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+          .AsQueryable();
+
+      switch (messageParams.MessageContainer)
+      {
+        case "Inbox":
+          messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.RecipientDeleted == false);
+          break;
+        case "Outbox":
+          messages = messages.Where(u => u.SenderId == messageParams.UserId && u.SenderDeleted == false);
+          break;
+        default:
+          messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false && u.RecipientDeleted == false);
+          break;
+      }
+
+      messages = messages.OrderByDescending(d => d.MessageSent);
+
+      return await PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+    }
+
+    public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+    {
+      var messages = await _context.Messages
+          .Include(u => u.Sender).ThenInclude(p => p.Photos)
+          .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+          .Where(m => (m.RecipientId == userId && m.RecipientDeleted == false && m.SenderId == recipientId)
+              || (m.RecipientId == recipientId && m.SenderId == userId && m.SenderDeleted == false))
+          .OrderByDescending(m => m.MessageSent)
+          .ToListAsync();
+
+      return messages;
+    }
+
+    public async Task<IEnumerable<Photo>> GetPhotosForUser(int userId)
+    {
+      var photos = await _context.Photos.Include(p => p.Id == userId).ToListAsync();
+      return photos;
+    }
+  }
 }
